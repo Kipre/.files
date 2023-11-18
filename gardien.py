ddd9d53ad5a8874daac0563a2d24929ed900e7fb
Cyprien <kipre>
2023-11-18 23:17:27 +0000

    add force flag

--- gardien.py
+++ gardien.py
@@ -5,8 +5,8 @@ import tomllib
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
-def create_symlinks(dry_run=False):
-    with open("links.toml", "rb") as f:
+def create_symlinks(dry_run=False, force=False):
+    with open(dir_path + "/links.toml", "rb") as f:
         config = tomllib.load(f)
     
     # other platforms are not supported
@@ -23,14 +23,21 @@ def create_symlinks(dry_run=False):
         destination = f"{item[platform]}/{item['name']}".replace("~", home)
         source = f"{dir_path}/{item['name']}"
         
+        is_not_empty = os.path.exists(destination) 
+        is_already_linked = os.path.islink(destination)
+
         if dry_run:
-            if os.path.islink(destination):
+            if is_already_linked:
                 print(f"{destination} is already a symlink")
 
-            elif os.path.exists(destination):
-                print(f"Warning {destination} is not empty")
+            elif is_not_empty:
+                print(f"Warning: {destination} is not empty")
 
             continue
+
+        if force and is_already_linked:
+            print("Removing existing link")
+            os.unlink(destination)
 
         try:
             os.symlink(source, destination)
@@ -47,6 +54,9 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('--dry-run', dest='dry_run', action='store_true',
                     help='do not do anything, just run checks')
+
+parser.add_argument('--force', dest='force', action='store_true',
+                    help='overwrite existing links')
 
 if __name__ == "__main__":
     args = parser.parse_args()
