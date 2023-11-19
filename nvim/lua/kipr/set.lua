4ca8e4da114b1ee4d2970b017d19ccfef082bc2d
Cyprien <cyprien.neverov@d3s-consulting.com>
2023-11-19 00:28:09 +0000

    adjustments for linux

--- nvim/lua/kipr/set.lua
+++ nvim/lua/kipr/set.lua
@@ -5,7 +5,7 @@ vim.opt.incsearch = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("USERPROFILE") .. "/.vim/undodir"
+vim.opt.undodir = (os.getenv("USERPROFILE") or os.getenv("HOME")) .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.termguicolors = true
