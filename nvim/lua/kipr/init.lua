55a064972c5071d67b70ab1b4f9523338de0c4ac
Cyprien <cyprien@kipr.cat>
2025-08-19 11:47:39 +0200

    add root dir finder bypass for my node-less projects

--- nvim/lua/kipr/init.lua
+++ nvim/lua/kipr/init.lua
@@ -5,7 +5,19 @@ vim.cmd [[
   au TextYankPost * silent! lua vim.highlight.on_yank {}
 ]]
 
+
 vim.lsp.enable({'ts_ls', 'biome', 'zls', 'clangd'})
+
+function find_git_root(bufnr, on_dir)
+  project_root = vim.fs.root(bufnr, {'.git'});
+  if not project_root then
+    return
+  end
+  return on_dir(project_dir)
+end
+
+vim.lsp.config('biome', { root_dir = find_git_root })
+vim.lsp.config('ts_ls', { root_dir = find_git_root })
 
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(ev)
